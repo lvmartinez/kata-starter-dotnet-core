dfcdd04064f6cd467bd92b4ffc506e185734d60f
luciavmartinez <lucia.martinez@ymail.com>
2020-02-06 12:01:14 -0600

    kata calculator when adding unknown numbers

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
 namespace Kata
 {
     public class Calculator
@@ -6,8 +10,21 @@ namespace Kata
         {
             if(string.IsNullOrEmpty(number))
                 return 0;
+   
+            var sumNumbers = number.Split(",");
 
-            return 3;
+            if (sumNumbers.Length == 1)
+                return int.Parse(sumNumbers[0]);
+            
+            var numberArr = new List<int>();
+            
+            foreach (var num in sumNumbers)
+            {
+                numberArr.Add(int.Parse(num));
+            }
+
+            var sum = numberArr[0]+numberArr[1];
+            return sum;
         }
     }
 }
